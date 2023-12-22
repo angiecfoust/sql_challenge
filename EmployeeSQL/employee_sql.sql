544c9fffe313b96a5b045ad66656f79b50930397
Angie Foust <angiecfoust@gmail.com>
2023-12-21 18:37:48 -0600

    created tables

---
+++ EmployeeSQL/employee_sql.sql
@@ -0,0 +1,56 @@
+-- Create 6 tables for csv files, then import data
+
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR,
+	birth_date DATE,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	sex VARCHAR,
+	hire_date DATE
+	);
+	
+CREATE TABLE departments (
+	dept_no VARCHAR PRIMARY KEY,
+	dept_name VARCHAR
+	);
+
+CREATE TABLE dept_emp (
+	emp_no INT,
+	dept_no VARCHAR,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+	);
+
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR,
+	emp_no INT,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	);
+
+
+CREATE TABLE salaries (
+	emp_no INT,
+	salary INT,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	);
+
+
+CREATE TABLE titles (
+	title_id VARCHAR PRIMARY KEY,
+	title VARCHAR);
+
+ALTER TABLE employees
+ADD FOREIGN KEY (emp_title_id) REFERENCES titles(title_id);
+
+--Test table creation
+SELECT * FROM employees;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+SELECT * FROM departments;
+
+-- DON'T RUN ANYTHING ABOVE THIS --
