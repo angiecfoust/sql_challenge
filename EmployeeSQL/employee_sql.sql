8daf7ad3f86a31c05a63c7cb9e3ca3bbc519858f
Angie Foust <angiecfoust@gmail.com>
2023-12-31 13:19:15 -0600

    intial work on data analysis questions

--- EmployeeSQL/employee_sql.sql
+++ EmployeeSQL/employee_sql.sql
@@ -55,3 +55,34 @@ SELECT * FROM departments;
 
 --CSV FILES HAVE ALL BEEN IMPORTED AND TABLES CHECKED--
 -- DON'T RUN ANYTHING ABOVE THIS --
+
+-- List the employee number, last name, first name, sex, and salary of each employee.
+--THIS IS NOT WORKING
+
+SELECT emp_no, last_name, first_name, sex
+FROM employees
+INNER JOIN salaries
+ON salaries.emp_no = employees.emp_no;
+
+
+
+
+-- List the first name, last name, and hire date for the employees who were hired in 1986.
+
+
+-- List the manager of each department along with their department number, department name, employee number, last name, and first name.
+
+
+-- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+
+
+-- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
+
+-- List each employee in the Sales department, including their employee number, last name, and first name.
+
+
+-- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+-- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
