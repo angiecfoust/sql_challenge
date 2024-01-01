5ad36f1ee2bc577bce53a2cf7f97b9cd3516cc1a
Angie Foust <angiecfoust@gmail.com>
2024-01-01 17:19:32 -0600

    questions 1 and 2 completed

--- EmployeeSQL/employee_sql.sql
+++ EmployeeSQL/employee_sql.sql
@@ -57,21 +57,30 @@ SELECT * FROM departments;
 -- DON'T RUN ANYTHING ABOVE THIS --
 
 -- List the employee number, last name, first name, sex, and salary of each employee.
---THIS IS NOT WORKING
-
-SELECT emp_no, last_name, first_name, sex
+SELECT employees.emp_no, 
+	employees.last_name, 
+	employees.first_name, 
+	employees.sex,
+	salaries.salary
 FROM employees
 INNER JOIN salaries
-ON salaries.emp_no = employees.emp_no;
-
-
-
+ON employees.emp_no = salaries.emp_no;
 
 -- List the first name, last name, and hire date for the employees who were hired in 1986.
-
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1/1/1986' AND '12/31/1986';
 
 -- List the manager of each department along with their department number, department name, employee number, last name, and first name.
-
+-- EVERYTHING ABOVE IS WORKING; for this question wait for next class
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE emp_no IN
+	(SELECT emp_no, dept_no
+	FROM dept_manager
+	WHERE dept_no IN
+		(SELECT dept_no, dept_name
+		FROM departments));
 
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
 
