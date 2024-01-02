1db2a448b24f143cb091fdf2a60385ba7038023c
Angie Foust <angiecfoust@gmail.com>
2024-01-02 14:01:29 -0600

    Remaining questions answered- final

--- EmployeeSQL/employee_sql.sql
+++ EmployeeSQL/employee_sql.sql
@@ -54,7 +54,6 @@ SELECT * FROM titles;
 SELECT * FROM departments;
 
 --CSV FILES HAVE ALL BEEN IMPORTED AND TABLES CHECKED--
--- DON'T RUN ANYTHING ABOVE THIS --
 
 -- List the employee number, last name, first name, sex, and salary of each employee.
 SELECT employees.emp_no, 
@@ -72,26 +71,33 @@ FROM employees
 WHERE hire_date BETWEEN '1/1/1986' AND '12/31/1986';
 
 -- List the manager of each department along with their department number, department name, employee number, last name, and first name.
--- EVERYTHING ABOVE IS WORKING; for this question wait for next class
-SELECT emp_no, first_name, last_name
+SELECT employees.emp_no, employees.first_name, employees.last_name,
+	dept_manager.dept_no, 
+	departments.dept_name
 FROM employees
-WHERE emp_no IN
-	(SELECT emp_no, dept_no
-	FROM dept_manager
-	WHERE dept_no IN
-		(SELECT dept_no, dept_name
-		FROM departments));
+INNER JOIN dept_manager on employees.emp_no = dept_manager.emp_no
+INNER JOIN departments on dept_manager.dept_no = departments.dept_no;
+
 
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+SELECT employees.emp_no, employees.last_name, employees.first_name,
+	departments.dept_name
+FROM employees
+INNER JOIN dept_emp on employees.emp_no = dept_emp.emp_no
+INNER JOIN departments on dept_emp.dept_no = departments.dept_no; 
 
 
 -- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 
 -- List each employee in the Sales department, including their employee number, last name, and first name.
-
-
--- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
-
--- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE emp_no IN
+	(SELECT emp_no FROM dept_emp
+	WHERE dept_no IN
+		(SELECT dept_no FROM departments
+		WHERE dept_name = 'Sales'));
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sql_challenge
 Module 9 Graded Assignment
+
+Additional resources used outside of those provided in class.
+Help with joining multiple tables: https://www.w3schools.com/sql/sql_join_inner.asp
